35ef6251f5c880ba1305e32a6905933460d7628b
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-08-23 18:13:53 +0100

    Update madlibs.py

--- beginners/madlibs.py
+++ beginners/madlibs.py
@@ -1,5 +1,15 @@
 print("""
 Mad Libs Generator!
 
+Enter a number:
 1: Romeo and Juiet
+2: Bee Movie
+3: FitGram Pacer Test
 """)
+
+juliet = """A rose
+"""
+beemovie = """accor
+"""
+fitgram = """THe
+"""
