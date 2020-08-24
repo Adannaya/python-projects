571a10d918ffa9293a658f9cc18003a0353e8c77
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-08-24 15:42:29 +0100

    Complete email slicer

--- beginners/email_slicer.py
+++ beginners/email_slicer.py
@@ -1,2 +1,10 @@
 print("Email slicer!\nEnter an email address and the username and domain name will be returned to you\n")
 email = input("Enter an email address here: ")
+
+at_index = email.find("@")
+if at_index == -1:
+	print('Are you sure this is a valid email address? There is no "@" symbol.")
+else:
+	username = email[:at_index]
+	domain = email[at_index + 1:]
+	print(f"Username: {username}\nDomain name: {domain}")
