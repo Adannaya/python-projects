2f5a6e1ca08c0f14ab75ccee10bbde0a62d3e163
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-09-06 19:02:21 +0100

    Create ItsTurtleTime.py

---
+++ teachwithict/ItsTurtleTime.py
@@ -0,0 +1,16 @@
+from turtle import *
+shape("turtle")
+# square
+for i in range(4):
+    forward(100)
+    left(90)
+backward(100)
+# hexagon
+for i in range(6):
+    right(60)
+    forward(100)
+clear()
+# triangle
+for i in range(3):
+    forward(150)
+    left(120)
